c0fd9c4303e48794b2143393fee9d852f5977298
Studentaz <adstudentaz@gmail.com>
2017-04-10 22:22:46 +0700

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -55,7 +55,7 @@ jQuery(function() {
                     $search_results.append(appendString);
                 });
             } else {
-                $search_results.html('<li>No results found</li>');
+                $search_results.html('<li>Không có kết quả nào được tìm thấy</li>');
             }
         });
     }
